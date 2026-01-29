53a6d18805bc09979cf8746560aa3b4b56f2b6f9
Aaron Deibele <deibele1@gmail.com>
2026-01-28 19:46:40 -0800

    Writing code to seed database from csv

--- photos-api/db/seeds.rb
+++ photos-api/db/seeds.rb
@@ -7,3 +7,10 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
+require 'csv'
+
+CSV.foreach('../photos.csv', headers: true) do |row|
+  photographer = Photographer.create(photographer_id: row['photographer_id'], photographer: row['photographer'], photographer_url: row['photographer_url'])
+  image = photographer.images.create(external_id: row['id'], width: row['width'], height: row['height'], url: row['url'], avg_color: row['avg_color'], alt: row['alt'])
+  image.create_src(original: row['src.original'], large2x: row['src.large2x'], large: row['src.large'], medium: row['src.medium'], small: row['src.small'], portrait: row['src.portrait'], landscape: row['src.landscape'], tiny: row['src.tiny'])
+end
